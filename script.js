416fcaa8f8a593808aa1bf1cc3e66321cdf259fb
sanjo lawal <sanjolawal600@gmail.com>
2022-02-08 09:47:46 -0800

    je

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ function Hide() {
   menu1.style.display = "block";
   menu.style.display = "none";
 }
+
+
+
+
+// Pricing html script writing
+
+let web = document.querySelector('.price-p1')
+let software = document.querySelector('.price-p2')
+let box1 = document.querySelector("#box1");
+let box2 = document.querySelector("#box2");
+let box3 = document.querySelector("#box3");
+let box4 = document.querySelector("#box4");
+
+console.log(box1)
+
+software.addEventListener("click", Showsoft)
+
+function Showsoft() {
+    web.style.color = "black"
+    software.style.color = "red"
+    box1.style.display = "none"
+    box2.style.display = "none";
+    box3.style.display = "block";
+    box4.style.display = "block";
+}
+ 
+
+web.addEventListener("click", Showweb);
+
+function Showweb() {
+  web.style.color = "red";
+  software.style.color = "black";
+  box1.style.display = "block";
+  box2.style.display = "block";
+  box3.style.display = "none";
+  box4.style.display = "none";
+}
